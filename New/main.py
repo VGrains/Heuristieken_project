cf5d58f3f5106a00ef40a6998a91675b296a2f67
Kay Brouwers <kay.brouwers@live.nl>
2020-01-17 14:26:01 +0100

    last of the week babyyyyy

--- New/main.py
+++ New/main.py
@@ -1,9 +1,6 @@
 from classes import grid
 from functions import netlist, manhattan, plot
-from codefiles import astar
-
-
-
+from codefiles import astar, relax
 
 
 if __name__ == '__main__':
@@ -12,26 +9,28 @@ if __name__ == '__main__':
     obj = grid.Grid('print_1.csv')
     grid = obj.__repr__()
     
-    netlist = netlist.load_netlists('netlist_2.csv')
+    netlist = netlist.load_netlists('netlist_1.csv')
     
     gate_locations = obj.gates
     
     print(gate_locations)
  
-    
-    routes, copy_grid = astar.astar_grid(netlist, gate_locations, grid)
-    print(routes)
+    routes = astar.astar_grid(netlist, gate_locations, grid)
     for x in copy_grid:
         print('\n')
         for y in x:
             print(y)
      
-    ideal_score = sum(manhattan.manhattan_distance(gate_locations[route[0]][0], gate_locations[route[1]][0], gate_locations[route[0]][1], gate_locations[route[1]][1]) for route in netlist)
-    current_score = sum(len(routes[route]) - 1 for route in routes)
+    ideal_score = sum(manhattan.manhattan_distance(gate_locations[route[0]][1], gate_locations[route[1]][1], gate_locations[route[0]][2], gate_locations[route[1]][2]) for route in netlist)
+    current_score = sum(len(routes[route]) -1 for route in routes)
     
+    md_routes = {route:manhattan.manhattan_distance(gate_locations[route[0]][1], gate_locations[route[1]][1], gate_locations[route[0]][2], gate_locations[route[1]][2]) for route in netlist}
+    min_md_sorted = {k: v for k, v in sorted(md_routes.items(), key=lambda item: item[1])}
+    smallest_routes_first = [x for x in min_md_sorted]
+
     print("ideal: ", ideal_score)
     print("current: ", current_score)
-    
+
     plot.plot_3dgraph(gate_locations, routes)
 
     
