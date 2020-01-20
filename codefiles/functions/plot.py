dfe09184fbf8e3f5fae14abc1789f942d4557cf3
Dido Verstegen <didoverstegen123@gmail.com>
2020-01-20 14:22:43 +0100

    added relax algorithm

---
+++ codefiles/functions/plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+def plot_3dgraph(chip_locations, routes):
+    """ Creates an interactive 3d graph of the chips and circuits """
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    
+    # Add the chips as points
+    x = []
+    y = []
+
+    for i in chip_locations:
+        x.append(chip_locations[i][1])
+        y.append(chip_locations[i][2])
+
+    z = [0 for i in x]
+    plt.yticks(np.arange(min(y), max(y) + 1, 1.0))
+    plt.xticks(np.arange(min(x), max(x) + 1, 1.0))
+    ax.set_zticks([0, 1, 2, 3, 4, 5, 6, 7])
+    ax.scatter(x,y,z,s=75, c='r', marker='s')
+    
+    # Add the routes as wires
+    for x in routes:
+        wires_x = []
+        wires_y = []
+        wires_z = []
+        for y in routes[x]:
+            wires_x.append(y[1])
+            wires_y.append(y[2])
+            wires_z.append(y[0])
+
+        ax.plot(wires_x, wires_y, wires_z)
+    
+    
+
+
+    plt.show()
---
+++ main.py
@@ -0,0 +1,47 @@
+from codefiles.classes import grid
+from codefiles.functions import netlist, manhattan, plot, user_data, csvreader
+from codefiles.algoritmes import astar, relax
+
+
+if __name__ == '__main__':
+
+    # Create a grid from our data
+    obj = grid.Grid('data/chip_1/print_1.csv')
+    grid = obj.__repr__()
+    
+    netlist = netlist.load_netlists('data/chip_1/netlist_3.csv')
+    
+    gate_locations = obj.gates
+ 
+    routes, final_grid = relax.relax(netlist, gate_locations, grid)
+    
+    for x in routes:
+        print('\n')
+        for y in x:
+            print(y)
+     
+    ideal_score = sum(manhattan.manhattan_distance(gate_locations[route[0]][1], gate_locations[route[1]][1], gate_locations[route[0]][2], gate_locations[route[1]][2]) for route in netlist)
+    current_score = sum(len(routes[route]) -1 for route in routes)
+    
+
+    md_routes = {route:manhattan.manhattan_distance(gate_locations[route[0]][1], gate_locations[route[1]][1], gate_locations[route[0]][2], gate_locations[route[1]][2]) for route in netlist}
+    min_md_sorted = {k: v for k, v in sorted(md_routes.items(), key=lambda item: item[1])}
+    smallest_routes_first = [x for x in min_md_sorted]
+
+    print("ideal: ", ideal_score)
+    print("current: ", current_score)
+
+
+    collisions = relax.calculate_collisions(routes)
+    new_routes = routes
+    for x in collisions:
+        new_routes[x] = relax.to_the_sky(routes[x])
+        break
+
+    new_collisions = relax.calculate_collisions(new_routes)
+
+    plot.plot_3dgraph(gate_locations, new_routes)
+    
+
+    
+    
